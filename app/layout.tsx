8fe7326275e289cf47ec22bda12cca14a8967f9c
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-12 17:51:51 +0400

    Layout metadata && favicon and import Header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "@/components/layout/Header";
 
 export const metadata: Metadata = {
   title: "Momentum",
   description: "Momentum - Progress Tracking Software",
+  icons: '/images/favicon.ico'
 };
 
 export default function RootLayout({
@@ -14,6 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="font-firago">
+        <Header />
         {children}
       </body>
     </html>
