b10f7c42c86590d50386e4710866c5fc26eb4fcb
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-14 01:14:13 +0400

    configure next.config.ts to take next/image

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,7 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'momentum.redberryinternship.ge',
-        pathname: '/storage/employee-avatars/**',
+        pathname: '/storage/**',
       },
     ],
   },
