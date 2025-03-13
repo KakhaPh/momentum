585777b0f434ac0281fa330bee2ee1dbfa1155a3
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-14 01:13:18 +0400

    add utils for dynamic colors and formatting text

---
+++ components/utils/colors.ts
@@ -0,0 +1,22 @@
+export const statusColors = [
+    { bg: 'bg-yellowtext', border: 'border-yellowtext' },
+    { bg: 'bg-orangebg', border: 'border-orangebg' },
+    { bg: 'bg-pinkbg', border: 'border-pinkbg' },
+    { bg: 'bg-bluebg', border: 'border-bluebg' }
+];
+
+export const priorityColors: { [key: number]: string } = {
+    1: "border-greentext text-greentext",
+    2: "border-yellowtext text-yellowtext",
+    3: "border-redtext text-redtext",
+};
+
+export const departmentColors: { [key: number]: string } = {
+    1: "bg-pinktext",
+    2: "bg-yellowbg",
+    3: "bg-orangetext",
+    4: "bg-bluetext",
+    5: "bg-redtext",
+    6: "bg-purpletext",
+    7: "bg-validations",
+};
