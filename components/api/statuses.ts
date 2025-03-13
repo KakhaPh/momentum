23c3ea4d7b9aeeff411404de92f1df14ebcf4a1b
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-14 01:09:58 +0400

    request file path change & taks fetching

--- components/api/statuses.ts
+++ components/api/statuses.ts
@@ -1,9 +1,9 @@
-import { request } from "../utils/request";
+import { request } from "../services/request";
 
 export const fetchStatuses = async () => {
     try {
         const response = await request.get('statuses');
-        return response.data;        
+        return response.data;
     } catch (error) {
         console.error("Failed fetching statuses!", error);
         throw error;
