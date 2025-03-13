e8651e526ae36e850117c13a0c5b0da5eeb53e66
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-13 18:28:56 +0400

    fetch data departments && priorities

---
+++ components/api/department.ts
@@ -0,0 +1,11 @@
+import { request } from "../utils/request";
+
+export const fetchDepartments = async () => {
+    try {
+        const response = await request.get('departments');
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching departments', error);
+        throw error;
+    }
+}
