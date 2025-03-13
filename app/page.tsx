216562a5991719f49fefd947d6ba508720ec877f
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-13 18:27:37 +0400

    start creating first tasks page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Filter from "@/components/filter/Filter";
+
 export default function Home() {
   return (
-    <div className="text-3xl ">
+    <div className="">
+      <h1 className="font-firago font-semibold text-[34px] leading-none tracking-normal h-[41px] pt-3 pb-[90px] text-headlines">დავალებების გვერდი</h1>
+      <Filter />
     </div>
   );
 }
