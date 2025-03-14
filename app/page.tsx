adc1271615f8a30e21698025ad0c5f10ccb00758
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-14 23:45:38 +0400

    Finish filter cards

--- app/page.tsx
+++ app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <h1 className="font-firago font-semibold text-[34px] leading-none tracking-normal h-[41px] pt-3 pb-[90px] text-headlines">დავალებების გვერდი</h1>
       <FilterProvider>
         <Filter />
+        <Statuses />
       </FilterProvider>
-      <Statuses />
     </>
   );
 }
