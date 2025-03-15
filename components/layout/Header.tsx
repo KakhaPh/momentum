9445e982e4a746785d4aabef78633ac9a5e0aff8
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-15 19:32:32 +0400

    header modify

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import { PlusIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import CustomButtom from '../custom/CustomButton'
 import CustomButton from '../custom/CustomButton'
+import EmployerModal from '../Employee/EmployerModal'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
+    const router = useRouter();
     return (
         <header className="sticky top-0 w-full h-[100px] flex justify-between items-center lg:px-[120px] md:px-10 sm:px-6 xs:px-4 py-[30px] bg-white z-10">
             <div className='flex w-[210px] h-[38px] gap-1'>
@@ -25,11 +29,9 @@ const Header = () => {
             </div>
 
             <div className='hidden md:flex w-[533px] h-10 gap-10 font-firago font-normal text-headlines'>
+                <EmployerModal />
                 <Link href={'#'} >
-                    <CustomButton title='თანამშრომლის შექმნა' style='w-[225px] h-[39px]'/>
-                </Link>
-                <Link href={'#'} >
-                    <CustomButton title='შექმენი ახალი დავალება' fill icon={<PlusIcon />} style='w-[268px] h-10'/>
+                <CustomButton title='შექმენი ახალი დავალება' fill={true} icon={<PlusIcon />} style='w-[268px] h-10' onClick={() => router.push('#')}/>
                 </Link>
             </div>
 
