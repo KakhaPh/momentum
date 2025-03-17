2c3e8548fb698621f1a06e96f01f872e286f72a1
KakhaPh <kakha.putkaradze@eu.edu.ge>
2025-03-17 14:31:24 +0400

    schema update and fix

--- components/Schema/CreateTaskSchema.ts
+++ components/Schema/CreateTaskSchema.ts
@@ -2,20 +2,30 @@ import { z } from "zod";
 
 const CreateTaskSchema = z.object({
     name: z
-        .string()
-        .min(1, "სათაური სავალდებულოა")
-        .min(2, "მინიმუმ 2 სიმბოლო")
-        .max(255, "მაქსიმუმ 255 სიმბოლო"),
-    description: z
-        .string()
-        .min(1, "აღწერა სავალდებულოა")
-        .min(2, "მინიმუმ 2 სიმბოლო")
-        .max(255, "მაქსიმუმ 255 სიმბოლო"),
-    priority_id: z.number().nullable().refine(val => val !== null, "პრიორიტეტი სავალდებულოა"),
-    status_id: z.number().nullable().refine(val => val !== null, "სტატუსი სავალდებულოა"),
-    department_id: z.number().nullable().refine(val => val !== null, "დეპარტამენტი სავალდებულოა"),
-    employee_id: z.number().nullable().refine(val => val !== null, "თანამშრომელი სავალდებულოა"),
+    .string()
+    .min(2, "მინიმუმ 2 სიმბოლო")
+    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+    description: z.string()
+    .min(2, "მინიმუმ 2 სიმბოლო")
+    .max(255, "მაქსიმუმ 255 სიმბოლო"),
+    priority_id: z.number({
+        required_error: "გთხოვთ აირჩიოთ პრიორიტეტი",
+        invalid_type_error: "გთხოვთ აირჩიოთ პრიორიტეტი",
+    }),
+    status_id: z.number({
+        required_error: "გთხოვთ აირჩიოთ სტატუსი",
+        invalid_type_error: "გთხოვთ აირჩიოთ სტატუსი",
+    }),
+    department_id: z.number({
+        required_error: "გთხოვთ აირჩიოთ დეპარტამენტი",
+        invalid_type_error: "გთხოვთ აირჩიოთ დეპარტამენტი",
+    }),
+    employee_id: z.number({
+        required_error: "გთხოვთ აირჩიოთ თანამშრომელი",
+        invalid_type_error: "გთხოვთ აირჩიოთ თანამშრომელი",
+    }),
     due_date: z.string().min(1, "შესრულების თარიღი სავალდებულოა")
+
 });
 
 export type TaskSchema = z.infer<typeof CreateTaskSchema>;
